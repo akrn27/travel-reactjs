ff019735c85a629249a2bdbf7fdce38a825ba0bf
Akrn27 <akurnia755@gmail.com>
2021-10-12 21:03:56 +0700

    finish product, services and signup page and style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from  './components/pages/Home';
+import Services from './components/pages/Services.js';
+import Products from './components/pages/Products.js';
+import SignUp from './components/pages/SignUp.js';
+
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/sign-up' component={SignUp}></Route>
       </Switch>
     </Router>
     </>
