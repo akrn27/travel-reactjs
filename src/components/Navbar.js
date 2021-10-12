6c1f5425da4010344f9f6bd503594c295817e605
Akrn27 <akurnia755@gmail.com>
2021-10-12 23:14:56 +0700

    edit logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        TRVL <i className="fab fa-typo3" />
+                        KUYS <i className="fab fa-affiliatetheme" />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
