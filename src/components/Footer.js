9717b83fcf0c5c0a52ab9d1c9d37a1b06e10d070
Akrn27 <akurnia755@gmail.com>
2021-10-12 21:16:42 +0700

    add Footer.js and Footer.css file

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join the Adventure newsletter to receive our best vacation deals
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default Footer 
